790fd6b9400eadf06b8dc0bef0964e2c113ad889
fredericmarchand <fredericmarchand@cmail.carleton.ca>
2014-11-07 17:12:30 -0500

    Added testbench code

---
+++ Comp4109EncryptedSerial.ino/Comp4109EncryptedSerial.ino.ino
@@ -0,0 +1,23 @@
+//Input Text through console
+//Transmit through serial
+//On receive transmit the inverse
+//Print encryption step on console
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+
+void setup() {
+  Serial.begin(9600);
+  inputString.reserve(200);
+}
+
+void loop() 
+{
+  delay(5000); 
+  Serial.write("Hello World\r\n");
+}
+
+
+
+
+
---
+++ TestProgram/TestProgram.ino
@@ -0,0 +1,18 @@
+String inputString = "";         // a string to hold incoming data
+
+void setup() {
+  Serial.begin(9600);
+  inputString.reserve(200);
+}
+
+void loop() {
+  while (Serial.available()) {
+    delay(100);
+    char inChar = (char)Serial.read(); 
+    inputString += inChar;
+    if (inChar == '\n') {
+      Serial.print(inputString);
+      inputString = "";
+    } 
+  }
+}
