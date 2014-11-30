69cd4645ea0eca5aa4f1f418a6d50b5ce43a78ae
fredericmarchand <fredericmarchand@cmail.carleton.ca>
2014-11-30 18:33:29 -0500

    updated demo

--- Comp4109EncryptedSerial/Comp4109EncryptedSerial.ino/Comp4109EncryptedSerial.ino.ino
+++ Comp4109EncryptedSerial/Comp4109EncryptedSerial.ino/Comp4109EncryptedSerial.ino.ino
@@ -1,14 +1,9 @@
-//Initiate serial
-//Input Text through console
-//Transmit through serial
-//On receive transmit the inverse
-//Print encryption step on console
 #include <des.h>
 #include <EncSoftwareSerial.h>
 
 EncSoftwareSerial mySerial(10, 11, "ABCDEFGH"); // RX, TX
-char cipher[17];
-char plain[17];
+char cipher[64];
+char plain[64];
 
 DES des;
 void setup() {
@@ -20,8 +15,8 @@ void loop() {
   static int counter = 0;
   int charCount = 0;
 
-  memset(cipher, '\0', 16);
-  memset(plain, '\0', 16);
+  memset(cipher, '\0', 64);
+  memset(plain, '\0', 64);
     
   while (!mySerial.available());
   delay(100);
@@ -29,29 +24,44 @@ void loop() {
   {
     char inChar = (char)mySerial.read(); 
     cipher[charCount++] = inChar; 
-    if (charCount == 16)
-    {
-       Serial.println(cipher);
-       des.tripleDESDecrypt(plain, cipher, "ABCDEFGH", "ABCDEFGH", "ABCDEFGH");
-       for (int i = 0; i < 16; ++i)
-       {
-         if (plain[i] != '0')
-           Serial.print(plain[i]);
-       }
-       Serial.println();
-       charCount = 0;
-    }
   }
-    if (charCount == 8)
-    {
-       Serial.println(cipher);
-       des.tripleDESDecrypt(plain, cipher, "ABCDEFGH", "ABCDEFGH", "ABCDEFGH");
-       for (int i = 0; i < 8; ++i)
-       {
-         if (plain[i] != '0')
-           Serial.print(plain[i]);
-       }
-       Serial.println();
-       charCount = 0;
-    }
+  
+  if (charCount == 24)
+  {
+     Serial.println(cipher);
+     des.tripleDESDecrypt(plain, cipher, "ABCDEFGH", "ABCDEFGH", "ABCDEFGH");
+     for (int i = 0; i < 24; ++i)
+     {
+       if (plain[i] != '0')
+         Serial.print(plain[i]);
+     }
+     Serial.println();
+     charCount = 0;
+  }
+  
+  if (charCount == 16)
+  {
+     Serial.println(cipher);
+     des.tripleDESDecrypt(plain, cipher, "ABCDEFGH", "ABCDEFGH", "ABCDEFGH");
+     for (int i = 0; i < 16; ++i)
+     {
+       if (plain[i] != '0')
+         Serial.print(plain[i]);
+     }
+     Serial.println();
+     charCount = 0;
+  }
+    
+  if (charCount == 8)
+  {
+     Serial.println(cipher);
+     des.tripleDESDecrypt(plain, cipher, "ABCDEFGH", "ABCDEFGH", "ABCDEFGH");
+     for (int i = 0; i < 8; ++i)
+     {
+       if (plain[i] != '0')
+         Serial.print(plain[i]);
+     }
+     Serial.println();
+     charCount = 0;
+  }
 }
